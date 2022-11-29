531d68d8f4169eb6d2678b7d1492f1fecb003f2d
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-28 23:46:44 -0300

    10º Requisito

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
+const delButton = document.getElementById('apaga-tudo');
 const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
 const texto = document.getElementById('texto-tarefa');
 
-const addButton = () => {
-  button.addEventListener('click', () => {
-    const li = document.createElement('li');
-    li.innerText = texto.value;
-    ol.appendChild(li);
-    texto.value = '';
-    li.addEventListener('click', (e) => {
-      const liCreated = document.getElementsByTagName('li');
-      for (let i = 0; i < liCreated.length; i += 1) liCreated[i].style.backgroundColor = 'white';
-      e.target.style.backgroundColor = 'gray';
-    });
-    li.addEventListener('dblclick', (e) => {
-      if (e.target.className === 'completed') {
-        e.target.className = '';
-      } else {
-        e.target.className = 'completed';
-      }
-    });
-  });
+const liOneClick = (e) => {
+  const liCreated = document.getElementsByTagName('li');
+  for (let i = 0; i < liCreated.length; i += 1) {
+    liCreated[i].style.backgroundColor = 'white';
+  }
+  e.target.style.backgroundColor = 'gray';
 };
 
-addButton();
+const liDoubleClick = (e) => {
+  if (e.target.className === 'completed') {
+    e.target.className = '';
+  } else {
+    e.target.className = 'completed';
+  }
+};
+
+const addButton = () => {
+  const li = document.createElement('li');
+  li.innerText = texto.value;
+  li.className = 'test';
+  texto.value = '';
+  ol.appendChild(li);
+  li.addEventListener('click', liOneClick);
+  li.addEventListener('dblclick', liDoubleClick);
+};
+
+const apagar = () => {
+  const li = document.querySelectorAll('.test');
+  for (let i = 0; i < li.length; i += 1) {
+    ol.removeChild(li[i]);
+  }
+};
+
+button.addEventListener('click', addButton);
+delButton.addEventListener('click', apagar);
