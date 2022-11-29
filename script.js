ba8235c9189182e4efffb36011e0579d03931a57
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-28 22:46:57 -0300

    9º Requisito

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
 const texto = document.getElementById('texto-tarefa');
 
-function addButton() {
+const addButton = () => {
   button.addEventListener('click', () => {
     const li = document.createElement('li');
     li.innerText = texto.value;
     ol.appendChild(li);
     texto.value = '';
-    li.addEventListener('click', ({ target }) => {
+    li.addEventListener('click', (e) => {
       const liCreated = document.getElementsByTagName('li');
-      for (let i = 0; i < liCreated.length; i += 1) {
-        liCreated[i].style.backgroundColor = 'white';
+      for (let i = 0; i < liCreated.length; i += 1) liCreated[i].style.backgroundColor = 'white';
+      e.target.style.backgroundColor = 'gray';
+    });
+    li.addEventListener('dblclick', (e) => {
+      if (e.target.className === 'completed') {
+        e.target.className = '';
+      } else {
+        e.target.className = 'completed';
       }
-      target.style.backgroundColor = 'gray';
     });
   });
-}
+};
 
 addButton();
